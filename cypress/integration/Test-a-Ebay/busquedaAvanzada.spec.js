481e241344fecfa8fb47e7143211102b8a1b517f
CesarJMR <cesarjosemena@outlook.com>
2021-02-05 14:50:53 -0400

    B

--- cypress/integration/Test-a-Ebay/busquedaAvanzada.spec.js
+++ cypress/integration/Test-a-Ebay/busquedaAvanzada.spec.js
@@ -4,23 +4,25 @@ import {BusquedaAvanzada} from "../../page-objects/Ebay"
 
 describe('Búsqueda Avanzada', function(){
 
+    //Hacer esta prueba con la página de búsqueda avanzada de Ebay abierta
+
     const busquedaAvanzada = new BusquedaAvanzada()
 
     it('Should hacer una búsqueda avanzada', function(){
         busquedaAvanzada.navigate()
         busquedaAvanzada.busquedaPorPalabraYnumero('tenis','2', '888')
-        busquedaAvanzada.busquedaIncluyendo(2)
+        busquedaAvanzada.busquedaIncluyendo(6)
         busquedaAvanzada.precio(1000, 5000)
         busquedaAvanzada.formatoDeCompra(4)
-        busquedaAvanzada.estado(2)
+        busquedaAvanzada.estado(6)
         busquedaAvanzada.mostrarLosResultados1(4, '903', '12', '1000', '10000', '2000', '20000')
-        busquedaAvanzada.mostrarLosResultados2(5)
+        busquedaAvanzada.mostrarLosResultados2(11)
         busquedaAvanzada.opcionesDeEnvio()
-        busquedaAvanzada.ubicacion(3, '50', '35750', 'Europe', 'República Dominicana')
-        busquedaAvanzada.vendedores(3, 'Excluir', 'Marcos')
-        busquedaAvanzada.ordenarPor('Precio + Envío: más bajo primero')
-        busquedaAvanzada.verResultados('Galería de imágenes')
+        busquedaAvanzada.ubicacion(1, '50', '35750', '-1&saslc=2', '60')
+        busquedaAvanzada.vendedores(1, '2', 'Marcos')
+        busquedaAvanzada.ordenarPor('15')
+        busquedaAvanzada.verResultados('2')
         busquedaAvanzada.resultadosPorPaginas('25')
-        busquedaAvanzada.buscarOBorrarOpciones(1)
+        busquedaAvanzada.buscarOBorrarOpciones(2)
     })
 })
--- cypress/page-objects/Ebay.js
+++ cypress/page-objects/Ebay.js
@@ -3,134 +3,164 @@
 export class BusquedaAvanzada{
        
     navigate(){
-        cy.visit('https://www.ebay.com/sch/ebayadvsearch', {timeout: 10000})
+        cy.visit('https://ebay.com/sch/ebayadvsearch', {timeout: 10000})
     }
 
     busquedaPorPalabraYnumero(articulo, como, categoria){
-        cy.get('#_nkw').type(articulo)
-        cy.get('#_in_kw').select(como)
-        cy.get('#e1-1').select(categoria)
+        //articulo: escribir artículo que interesa buscar.
+        //como: ver dropdown en página y seleccionar opción por value.
+        //categoria: ver dropdown en página y seleccionar opción por value.
+        
+        cy.get('#_nkw').type(articulo).should('have.value', articulo)
+        cy.get('#_in_kw').select(como).should('have.value', como)
+        cy.get('#e1-1').select(categoria).should('have.value', categoria)
     }
 
     busquedaIncluyendo(incluyendo){
-        cy.get(`#adv_search_from fieldset:nth-child(3) label:nth-child(${incluyendo})`).click()
+        //incluyendo: colocar 2 para primera opción, 4 para la segunda y 6 para la tercera.
+
+        cy.get(`#adv_search_from fieldset:nth-child(3) label:nth-child(${incluyendo}) input`).click().should('be.checked')
     }
 
     precio(desde, hasta){
         cy.get('#_mPrRngCbx').click()
-        cy.get('#adv_search_from fieldset:nth-child(4) input:nth-child(4)').type(desde)
-        cy.get('#adv_search_from fieldset:nth-child(4) input:nth-child(5)').type(hasta)
+        cy.get('#adv_search_from fieldset:nth-child(4) input:nth-child(4)').type(desde).should('have.value', desde)
+        cy.get('#adv_search_from fieldset:nth-child(4) input:nth-child(5)').type(hasta).should('have.value', hasta)
     }
 
     formatoDeCompra(formato){
-        cy.get(`#adv_search_from fieldset:nth-child(5) label:nth-child(${formato})`).click()
+        //formato: 2 para la primera opción y 4 para la segunda.
+
+        cy.get(`#adv_search_from fieldset:nth-child(5) label:nth-child(${formato}) input`).click().should('be.checked')
     }
 
     estado(estado){
-        cy.get(`#adv_search_from fieldset:nth-child(6) label:nth-child(${estado})`).click()
+        //estado: 2 para la primera opción, 4 para la segunda y 6 para la tercera.
+
+        cy.get(`#adv_search_from fieldset:nth-child(6) label:nth-child(${estado}) input`).click().should('be.checked')
     }
 
     mostrarLosResultados1(div, a, b, desde1, hasta1, desde2, hasta2){
+        //Div: seleccionar las opciones del switch case para seleccionar una opción.
+        //a: mirar primer dropdown de anuncios para ver que opción tomar.
+        //b: mirar segundo dropdown de anuncios para ver que opción tomar.
+
         switch(div){
             case 2:
-                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true})
-                cy.get('#adv_search_from fieldset:nth-child(7) div:nth-child(2) select:nth-child(3)').select(a)
-                cy.get('#adv_search_from fieldset:nth-child(7) div:nth-child(2) select:nth-child(4)').select(b)
+                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true}).should('be.checked')
+                cy.get('#adv_search_from fieldset:nth-child(7) div:nth-child(2) select:nth-child(3)').select(a).should('have.value', a)
+                cy.get('#adv_search_from fieldset:nth-child(7) div:nth-child(2) select:nth-child(4)').select(b).should('have.value', b)
                 break
             
             case 3:
-                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true})
-                cy.get('#_sabdlo').type(desde1)
-                cy.get('#_sabdhi').type(hasta1)
+                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true}).should('be.checked')
+                cy.get('#_sabdlo').type(desde1).should('have.value', desde1)
+                cy.get('#_sabdhi').type(hasta1).should('have.value', hasta1)
                 break
 
             case 4:
-                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true})
-                cy.get('#_samilow').type(desde2)
-                cy.get('#_samihi').type(hasta2)
+                cy.get(`#adv_search_from fieldset:nth-child(7) div:nth-child(${div}) input`).click({multiple: true}).should('be.checked')
+                cy.get('#_samilow').type(desde2).should('have.value', desde2)
+                cy.get('#_samihi').type(hasta2).should('have.value', hasta2)
                 break
         }
     }
 
     mostrarLosResultados2(label){
+        //label: seleccionar las opciones del switch case para seleccionar una opción.
+
         switch(label){
             case 5:
-                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true})
+                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true}).should('be.checked')
                 break
 
             case 7:
-                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true})
+                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true}).should('be.checked')
                 break
 
             case 9:
-                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true})
+                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true}).should('be.checked')
                 break
 
             case 11:
-                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true})
+                cy.get(`#adv_search_from fieldset:nth-child(7) label:nth-child(${label}) input`).click({multiple: true}).should('be.checked')
                 break
         }
     }
 
     opcionesDeEnvio(){
-        cy.get('#adv_search_from fieldset:nth-child(7) fieldset label input').click()
+        cy.get('#adv_search_from fieldset:nth-child(7) fieldset label input').click().should('be.checked')
     }
 
     ubicacion(ubicacion, millas, zipcode, continente, pais){
+        //ubicacion: seleccionar opciones del switch case para seleccionar una opción.
+
         switch(ubicacion){
             case 1:
-                cy.get('#LH_Located').click()
-                cy.get('#_sadis').select(millas)
-                cy.get('#_stpos').type(zipcode)
+                cy.get('#LH_Located').click().should('be.checked')
+                cy.get('#_sadis').select(millas).should('have.value', millas)
+                cy.get('#_stpos').type(zipcode).should('have.value', zipcode)
                 break
             
             case 2:
-                cy.get('#LH_PrefLocRadio').click()
-                cy.get('#_sargnSelect').select(continente)
+                cy.get('#LH_PrefLocRadio').click().should('be.checked')
+                cy.get('#_sargnSelect').select(continente).should('have.value', continente)
                 break
 
             case 3:
-                cy.get('#LH_LocatedInRadio').click()
-                cy.get('#_salicSelect').select(pais)
+                cy.get('#LH_LocatedInRadio').click().should('be.checked')
+                cy.get('#_salicSelect').select(pais).should('have.value', pais)
                 break
         }
     }
 
     vendedores(opcion, inOex, vendedor){
-        cy.get('#_fss').click()
+        //opcion: seleccionar opciones del switch case para seleccionar una opción.
+        //inOe: incluído o excluído (1 o 2)
+
+        cy.get('#_fss').click().should('be.checked')
         switch(opcion){
             case 1:
-                cy.get('#LH_SpecificSeller_id').click()
-                cy.get('#_saslop').select(inOex)
-                cy.get('#_sasl').type(vendedor)
+                cy.get('#LH_SpecificSeller_id').click().should('be.checked')
+                cy.get('#_saslop').select(inOex).should('have.value', inOex)
+                cy.get('#_sasl').type(vendedor).should('have.value', vendedor)
                 break
 
             case 2:
-                cy.get('#LH_FavSellers_id').click()
+                cy.get('#LH_FavSellers_id').click().should('be.checked')
                 break
 
             case 3:
-                cy.get('#LH_SellerWithStore_id').click()
+                cy.get('#LH_SellerWithStore_id').click().should('be.checked')
                 break
         }
     }
 
     ordenarPor(ordenar){
-        cy.get('#LH_SORT_BY').select(ordenar)
+        //ordenar: ver dropdown en página y seleccionar opción por value.
+
+        cy.get('#LH_SORT_BY').select(ordenar).should('have.value', ordenar)
     }
 
     verResultados(verResultados){
-        cy.get('#LH_VIEW_RESULTS_AS').select(verResultados)
+        //verResultados: ver dropdown en página y seleccionar opción por value.
+
+        cy.get('#LH_VIEW_RESULTS_AS').select(verResultados).should('have.value', verResultados)
     }
 
     resultadosPorPaginas(cantidad){
-        cy.get('#LH_IPP').select(cantidad)
+        //cantidad: ver dropdown en página y seleccionar opción por value.
+
+        cy.get('#LH_IPP').select(cantidad).should('have.value', cantidad)
     }
 
-    buscarOBorrarOpciones(buscarOBorrarOpciones){
-        switch(buscarOBorrarOpciones){
+    buscarOBorrarOpciones(buscarOborrarOpciones){
+        //buscarOborrarOpciones: seleccionar 1 para buscar y 2 para borrar opciones.
+
+        switch(buscarOborrarOpciones){
             case 1:
-                cy.get('#searchBtnLowerLnk').click()
+                //"chromeWebSecurity": false
+                cy.get('#searchBtnLowerLnk', {timeout:10000}).click()
                 break
 
             case 2:
